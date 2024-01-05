0242edebdebc7ca035cc9e7d936c31755d6d7e57
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2024-01-05 12:06:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        html_code = request.form['html_code']
+        html_code = request.form.get('html_code', '')
         rendered_html = Markup(html_code)
         return render_template('index.html', html_code=html_code, rendered_html=rendered_html)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Run the app on all available network interfaces with port 5000
     app.run(host='0.0.0.0', port=5000, debug=True)
